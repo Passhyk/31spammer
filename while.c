667595b6a9119f00b607092f366da21a6968e928
Passhyk <70662829+Passhyk@users.noreply.github.com>
2020-12-25 23:12:51 +0300

    Add files via upload

---
+++ while.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+
+int number = 1;
+int number2 = 31;
+
+     do {
+    printf("%d\n",number);
+    number++;
+    
+}
+
+
+while( number = number2);
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
